adfcb29e0ac41ebd8df6b7182d6d91a3ebb0ae6c
171727479@fbstudent.org.za <171727479@fbstudent.org.za>
2020-09-27 11:19:30 +0200

    Add project files.

---
+++ Database/Form1.Designer.vb
@@ -0,0 +1,290 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtTestMark = New System.Windows.Forms.TextBox()
+        Me.txtPracMark = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnCapture = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnTesCapture = New System.Windows.Forms.Button()
+        Me.btnTesEdit = New System.Windows.Forms.Button()
+        Me.btnTesDelete = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtStudNumber = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(37, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Student Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(37, 116)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 19)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Student Surname:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(40, 228)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 19)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Test Mark:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(20, 267)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(102, 19)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Practical Mark:"
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(150, 70)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(157, 26)
+        Me.txtName.TabIndex = 2
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSurname.Location = New System.Drawing.Point(150, 109)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(157, 26)
+        Me.txtSurname.TabIndex = 2
+        '
+        'txtTestMark
+        '
+        Me.txtTestMark.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTestMark.Location = New System.Drawing.Point(128, 221)
+        Me.txtTestMark.Name = "txtTestMark"
+        Me.txtTestMark.Size = New System.Drawing.Size(143, 26)
+        Me.txtTestMark.TabIndex = 2
+        '
+        'txtPracMark
+        '
+        Me.txtPracMark.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPracMark.Location = New System.Drawing.Point(128, 260)
+        Me.txtPracMark.Name = "txtPracMark"
+        Me.txtPracMark.Size = New System.Drawing.Size(143, 26)
+        Me.txtPracMark.TabIndex = 2
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Location = New System.Drawing.Point(291, 22)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(98, 32)
+        Me.btnSearch.TabIndex = 3
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(263, 141)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(88, 35)
+        Me.btnDelete.TabIndex = 4
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.Location = New System.Drawing.Point(157, 141)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(82, 35)
+        Me.btnEdit.TabIndex = 5
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnCapture
+        '
+        Me.btnCapture.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCapture.Location = New System.Drawing.Point(32, 141)
+        Me.btnCapture.Name = "btnCapture"
+        Me.btnCapture.Size = New System.Drawing.Size(109, 35)
+        Me.btnCapture.TabIndex = 6
+        Me.btnCapture.Text = "Capture"
+        Me.btnCapture.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(505, 57)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(57, 19)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Display:"
+        '
+        'btnTesCapture
+        '
+        Me.btnTesCapture.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTesCapture.Location = New System.Drawing.Point(27, 317)
+        Me.btnTesCapture.Name = "btnTesCapture"
+        Me.btnTesCapture.Size = New System.Drawing.Size(109, 35)
+        Me.btnTesCapture.TabIndex = 6
+        Me.btnTesCapture.Text = "Capture"
+        Me.btnTesCapture.UseVisualStyleBackColor = True
+        '
+        'btnTesEdit
+        '
+        Me.btnTesEdit.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTesEdit.Location = New System.Drawing.Point(152, 317)
+        Me.btnTesEdit.Name = "btnTesEdit"
+        Me.btnTesEdit.Size = New System.Drawing.Size(82, 35)
+        Me.btnTesEdit.TabIndex = 5
+        Me.btnTesEdit.Text = "Edit"
+        Me.btnTesEdit.UseVisualStyleBackColor = True
+        '
+        'btnTesDelete
+        '
+        Me.btnTesDelete.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTesDelete.Location = New System.Drawing.Point(258, 317)
+        Me.btnTesDelete.Name = "btnTesDelete"
+        Me.btnTesDelete.Size = New System.Drawing.Size(88, 35)
+        Me.btnTesDelete.TabIndex = 4
+        Me.btnTesDelete.Text = "Delete"
+        Me.btnTesDelete.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(500, 79)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(337, 290)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtStudNumber)
+        Me.GroupBox1.Controls.Add(Me.btnTesDelete)
+        Me.GroupBox1.Controls.Add(Me.btnSearch)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtSurname)
+        Me.GroupBox1.Controls.Add(Me.txtTestMark)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtPracMark)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.btnTesEdit)
+        Me.GroupBox1.Controls.Add(Me.btnTesCapture)
+        Me.GroupBox1.Controls.Add(Me.btnDelete)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.btnEdit)
+        Me.GroupBox1.Controls.Add(Me.btnCapture)
+        Me.GroupBox1.Location = New System.Drawing.Point(26, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(406, 377)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        '
+        'txtStudNumber
+        '
+        Me.txtStudNumber.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtStudNumber.Location = New System.Drawing.Point(128, 26)
+        Me.txtStudNumber.Name = "txtStudNumber"
+        Me.txtStudNumber.Size = New System.Drawing.Size(157, 26)
+        Me.txtStudNumber.TabIndex = 7
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(20, 26)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(112, 19)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Student Number:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(980, 475)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents txtTestMark As TextBox
+    Friend WithEvents txtPracMark As TextBox
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnCapture As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnTesCapture As Button
+    Friend WithEvents btnTesEdit As Button
+    Friend WithEvents btnTesDelete As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtStudNumber As TextBox
+End Class
---
+++ Database/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Data.OleDb
+
+
+'Imports System.Data.SqlClient
+'Imports System.Data.Odbc
+
+Public Class Form1
+    'database path = xxxxx
+    Dim OLEDBConnStr As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Z:\2020\CPRG\Sunday class/FalsebayCollege.accdb"
+    Dim OLEDBConn As New OleDb.OleDbConnection(OLEDBConnStr)
+
+
+    'Dim odbcConnStr As String = "Driver={Microsoft access Driver(*.mdb *.accdb)}; DBQ=Path/.extension"
+
+    'Dim conn As New SqlClient.SqlConnection("")
+    ' Dim con As New Odbc.OdbcConnection(odbcConnStr)
+
+
+    Dim com As New OleDb.OleDbCommand
+
+
+    Dim studentRecordsTable As New Data.DataTable
+    Dim marksTable As New Data.DataTable
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'con.ConnectionString = conStr
+        com.Connection = OLEDBConn
+
+        Try
+
+
+            com.CommandText = "SELECT *FROM studentRecordsTable"
+            'open the connection
+            OLEDBConn.Open()
+
+            studentRecordsTable.Load(com.ExecuteReader())
+            MessageBox.Show("it loaded")
+
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message)
+
+        Finally
+            'closed the conncetion
+            ' con.close()
+            'reset sql statement
+
+        End Try
+
+
+
+
+    End Sub
+
+    Private Sub btnCapture_Click(sender As Object, e As EventArgs) Handles btnCapture.Click
+        Try
+
+
+            com.CommandText = "INSERT INTO StudentRecords(StudentNumber,FirstName,Surname) VALUES ('1719','Try','Catch',)"
+
+            com.CommandText = "INSERT INTO StudentRecords(StudentNumber,FirstName,Surname) VALUES (@StudentNumber,@FirstName,@Surname)"
+
+
+
+            'open the connection
+
+            OLEDBConn.Open()
+
+            com.Parameters.AddWithValue("@StudentNumber", txtStudNumber.Text)
+            com.Parameters.AddWithValue("@FirstName", txtName)
+            com.Parameters.AddWithValue("@Surname", txtSurname)
+
+            'adds records into database
+            com.ExecuteNonQuery()
+
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message)
+
+        Finally
+            'closed the conncetion
+            OLEDBConn.Close()
+            'reset sql statement
+            com.Parameters.Clear()
+
+
+        End Try
+    End Sub
+End Class
